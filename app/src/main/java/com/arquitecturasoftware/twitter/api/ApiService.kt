a24c8417ef3a2775d42123c54064c71c29396afd
AC-Santiago <acostasantiago311124578@gmail.com>
2025-02-22 01:37:15 -0500

    Add: new url update user

--- app/src/main/java/com/arquitecturasoftware/twitter/api/ApiService.kt
+++ app/src/main/java/com/arquitecturasoftware/twitter/api/ApiService.kt
@@ -17,6 +17,7 @@ import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Part
 import retrofit2.http.Path
 
@@ -77,5 +78,10 @@ interface ApiService {
         @Header("Authorization") token: String,
         @Body tweetRequest: TweetRequest
     ): Response<TweetResponse>
+
+    @PUT("/profile/")
+    suspend fun updateProfile(
+        @Body profileUpdateRequest: ProfileUpdateRequest
+    ): Response<UsersProfileResponse>
 }
 
---
+++ app/src/main/java/com/arquitecturasoftware/twitter/api/response/ProfileUpdateRequest.kt
@@ -0,0 +1,6 @@
+package com.arquitecturasoftware.twitter.api.response
+
+data class ProfileUpdateRequest(
+    var name: String,
+    var password: String,
+)
